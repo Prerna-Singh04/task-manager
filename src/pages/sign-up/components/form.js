81d27bd81086161c7b22708de6367f21c61f436e
Prerna-Singh04 <prernasingh786.p@gmail.com>
2020-12-06 14:27:50 +0530

    changes in signup form handling

--- src/pages/sign-up/components/form.js
+++ src/pages/sign-up/components/form.js
@@ -1,19 +1,50 @@
 import React from "react";
 
-const SingnUpForm = () => {
-  return (
-    <div className="registration">
-        <h5>Sign Up</h5>
-      <form className="registration_form">
-          <input type = "string" className="registration_form_input" placeholder="First Name *" value="" required />
-          <input type = "string" className="registration_form_input"  placeholder="Last Name (optional) " value="" required/>
-          <input type = "string"  className="registration_form_input" placeholder="User Name *" value="" required />
-          <input type = "password" className="registration_form_input"  placeholder="Password *" value="" required />
-          <input type = "password" className="registration_form_input"  placeholder="Confirm Password *" value="" required />
-          <button className="registration_form_submit">Submit</button>
-      </form>
-      <p><a href="/">Login</a></p>
-    </div>
-  );
+class  SingnUpForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: null,
+            lastName: null,
+            userName: null,
+            password: null
+        }
+    }
+    
+    handleForm = (event)=> {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    registerUser = ()=> {
+        const { firstName, lastName, userName, password} = this.state;
+
+        const userData = { firstName, lastName, userName, password};
+        console.log('userData to register is ===>', userData);
+        
+        localStorage.setItem('userDetails', JSON.stringify(userData));
+        console.log(JSON.parse(localStorage.getItem('userDetails')));
+    }
+    render(){
+        const stateData = this.state;
+        return (
+            <div className="registration">
+                <h5>Sign Up</h5>
+              <div className="registration_form">
+                  <input type="text" name="firstName"  className="registration_form_input" placeholder="First Name *" value={stateData.firstName} onChange={this.handleForm}  />
+                  <input type="text" name="lastName" className="registration_form_input"  placeholder="Last Name (optional) " value={stateData.lastName}   onChange={this.handleForm} />
+                  <input type="text"  name="userName" className="registration_form_input" placeholder="User Name *"  value={stateData.userName} onChange={this.handleForm}  />
+                  <input type="password" name="password" className="registration_form_input"  placeholder="Password *" value={stateData.password} onChange={this.handleForm}  />
+                  <input type="password" className="registration_form_input"  placeholder="Confirm Password *"  onChange={this.handleForm}  />
+                  <button className="registration_form_submit" onClick={this.registerUser}>Submit</button>
+              </div>
+              <p><a href="/">Login</a></p>
+            </div>
+        );
+    }
+  
 };
 export default SingnUpForm;
