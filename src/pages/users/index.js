99ecbc9c0b502c0418d0ac2d4b2e65a3fee8d0fb
prerna-singh04 <prernasingh786.p@gmail.com>
2020-12-10 11:09:40 +0530

    users data display

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import Header from '../../common/components/header'
 
 const Users = () => {
-    return <><Header redirectUrl="/task" name="tasks"  /> page</>
+    const usersData = JSON.parse(localStorage.getItem("users"));
+    return <>
+        <Header redirectUrl="/task" name="tasks"/> 
+        <table className="task-table">
+        <tr className="table-column">
+            <th>First Name</th>
+            <th>last Name</th>
+            <th>userName</th>
+        </tr>
+        {usersData.map((item) => {
+            return (
+            <tr>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td>{item.userName}</td>
+            </tr>
+            );
+        })}
+        </table>
+    </>
 }
 
 export default Users;
